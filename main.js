729172ba3fc35d7b98ce57e69bcc6e86390b0567
Apri-maulana-nurharyanto <maulanaapri316@gmail.com>
2024-05-24 18:29:26 +0700

    update algoritma dan desain

--- main.js
+++ main.js
@@ -1,13 +1,36 @@
 const form = document.getElementById("form");
-var imgSource = document.getElementById("showImg");
+const popup = document.getElementById("popup");
+const btnSubmit = document.getElementById("btn-submit");
+
+function showPopup() {
+  popup.style.display = "block";
+  form.style.display = "none";
+}
+
+function hidePopup() {
+  location.reload();
+  popup.style.display = "none";
+}
+
+function toggleSubmitButton(isLoading) {
+  if (isLoading) {
+    btnSubmit.textContent = "Loading...";
+    btnSubmit.disabled = true;
+  } else {
+    btnSubmit.textContent = "Kirim";
+    btnSubmit.disabled = false;
+  }
+}
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  toggleSubmitButton(true)
   const file = form.file.files[0];
   const fr = new FileReader();
   fr.readAsArrayBuffer(file);
   fr.onload = (f) => {
-    const url = 
-      "https://script.google.com/macros/s/AKfycbydCKesNT1DjQ7fWV56D6eRGew1UKGQI4erUvT7ypGYNnmWmfkJNSefwsrxlJCv7d70/exec"; // <--- URL Web appscript setelah kalian deploy si appscriptnya
+    const url =
+      "https://script.google.com/macros/s/AKfycbzpyBQVmsdGVR4KAPvaKrjuDKII_1gaX9PYpgrTmJ7SvqEgyWuDSE9aFSw4uQbVlwMK/exec"; // <--- URL Web appscript setelah kalian deploy si appscriptnya
 
     const qs = new URLSearchParams({
       filename: form.filename.value || file.name,
@@ -20,9 +43,11 @@ form.addEventListener("submit", (e) => {
       .then((res) => res.json())
       .then((e) => {
         console.log(e); //appscript Response
-        document.getElementById("showImg").src =
-          "https://drive.google.com/thumbnail?id=" + e.fileId + "&sz=w1000"; //ini untuk pasang img src nya
       }) // response appscript
-      .catch((err) => console.log(err));
+      .catch((error) => {
+        console.log(error);
+        toggleSubmitButton(false);
+        showPopup();
+      });
   };
 });
